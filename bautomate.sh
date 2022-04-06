c5b8f843cc3af155565b24cac8791d9c410fc7d0
nicholasob7 <101837624+nicholasob7@users.noreply.github.com>
2022-04-07 09:45:52 +1200

    Update bautomate.sh

--- bautomate.sh
+++ bautomate.sh
@@ -1,7 +1,7 @@
 # run with :   curl https://raw.githubusercontent.com/nicholasob7/automate_linux_bash/main/automate_linux_shell?token=GHSAT0AAAAAABTAWJF5AC2DDUK6M5DUBD6YYSNBSOQ | bash
 #!/bin/bash
 
-echo "hello Nic"
+echo "Greetings Romulan"
 sudo yum install -y tmux fish python3 zsh file
 sudo pip3 install pylint pytest bpython
 echo "set-option repeat-time 10" >> ~/.tmux.conf
