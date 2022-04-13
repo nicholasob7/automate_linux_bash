76d77ef7c3e1a218ef72af08ea345c27a3316d88
nicholasob7 <101837624+nicholasob7@users.noreply.github.com>
2022-04-13 22:37:31 +1200

    Update bautomate.sh

--- bautomate.sh
+++ bautomate.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Usage: curl https://raw.githubusercontent.com/kris-classes/restart/main/shell_scripts/automated.sh | bash
 #
-# run with :   curl https://github.com/nicholasob7/automate_linux_bash/blob/main/bautomate.sh | bash
-echo "Bash script to install all necessary packages."
+# run with : curl https://github.com/nicholasob7/automate_linux_bash/blob/main/bautomate.sh | bash
 alias la="ls -al"
 sudo yum install -y tmux python3 fish strace zsh file gcc netcat tcpdump ftop
-sudo pip3 install pylint pytest bpython
+pip3 install pylint pytest bpython
 echo "set-option repeat-time 10" >> ~/.tmux.conf
