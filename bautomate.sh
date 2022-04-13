68fc933ab3dcd7bd4321358bd68a724ea3d0962d
nicholasob7 <101837624+nicholasob7@users.noreply.github.com>
2022-04-13 22:12:05 +1200

    added utilities to bautomate

--- bautomate.sh
+++ bautomate.sh
@@ -1,6 +1,7 @@
 # run with :   curl https://raw.githubusercontent.com/nicholasob7/automate_linux_bash/main/bautomate.sh |bash
 
-echo "Greetings Romulan"
+
 sudo yum install -y tmux fish python3 zsh file
+sudo yum install -y iftop tcpdump lsof whois netcat
 sudo pip3 install pylint pytest bpython
 echo "set-option repeat-time 10" >> ~/.tmux.conf
