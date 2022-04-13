ba5423ba250585ee4db8b2680dffa963fc3fe7fb
nicholasob7 <101837624+nicholasob7@users.noreply.github.com>
2022-04-13 22:41:23 +1200

    Update bautomate.sh

--- bautomate.sh
+++ bautomate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Usage: curl https://raw.githubusercontent.com/kris-classes/restart/main/shell_scripts/automated.sh | bash
 #
-# run with : curl https://github.com/nicholasob7/automate_linux_bash/blob/main/bautomate.sh | bash
+# run with : curl https://raw.githubusercontent.com/nicholasob7/automate_linux_bash/main/bautomate.sh | bash
 echo "Bash script to install all necessary packages."
 alias la="ls -al"
 sudo yum install -y tmux python3 fish strace zsh file gcc netcat tcpdump
